a8e6e61ac9e0595d76cdf6031ea7ae381103dcc0
tanvhla258 <tanvhla258@gmail.com>
2023-03-26 22:39:44 +0700

    fix to deploy

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
+
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { github } from "../assets";
